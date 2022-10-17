857b6c01083730d33f2a183c155d5922b1307356
Sumeet Godse <sumeetgodse1997@gmail.com>
2022-10-17 22:56:53 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import resume from "./resume.png"
 import "./css/home.css"
 
 export default function HomePage() {
     return (
         <div className="home-page">
-            <img src={resume} className="resume" />
         </div>
     )
 }
